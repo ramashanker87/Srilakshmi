cd3167fc94cc15346e1aee4f3e8a95c21718c429
srilakshmi <shriya1827@gmail.com>
2025-01-29 22:42:56 -0800

    Day08 commit

---
+++ Day08/spring-employee-app/src/main/java/com/srilakshmi/app/model/Employee.java
@@ -0,0 +1,34 @@
+package com.srilakshmi.app.model;
+
+public class Employee {
+    public String getId() {
+        return id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    String id;
+    String name;
+    int age;
+    public Employee() {}
+    public Employee(String id, String name, int age) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
