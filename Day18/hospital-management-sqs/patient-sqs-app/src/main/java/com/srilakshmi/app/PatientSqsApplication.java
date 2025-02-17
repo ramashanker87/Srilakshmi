ed172caf22d97b80c85aebbf69be1e8d6bf93d9c
admin <rama@gail.com>
2025-02-16 19:27:58 -0800

    Day18 commit

---
+++ Day18/hospital-management-sqs/patient-sqs-app/src/main/java/com/srilakshmi/app/PatientSqsApplication.java
@@ -0,0 +1,13 @@
+package com.srilakshmi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientSqsApplication.class, args);
+	}
+
+}
