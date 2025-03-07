53cff32232ab6331090b6cedf63827aa992df851
admin <rama@gail.com>
2025-03-06 20:28:54 -0800

    Day15 reference

---
+++ Day15/spring-rest-communication/src/main/java/com/srilakshmi/app/SpringRestCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.srilakshmi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCommunicationApplication.class, args);
+	}
+
+}
